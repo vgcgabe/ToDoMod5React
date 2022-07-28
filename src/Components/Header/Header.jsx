c8e85609af1a3620f285cd2a9c99c022322a9393
VictorGuedes <victor.guedes@flieger.software>
2022-07-27 23:40:31 -0300

    Changed Header jsx and css

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -5,23 +5,19 @@ import S from "./Header.modules.css"
 const Header = () => {
   return (
     <header>
-        <section>
+        <section className='textSection'>
           <h3>uma seleção de produtos</h3>
           <h1>especial para você</h1>
-        </section>
-
-        <section>
           <p>Todos os produtos dessa lista foram selecionados a partir da sua navegação. Aproveite!</p>
         </section>
 
         <section className='headerButtonsSection'>
-        < Button name="Botão teste props" />
-        < Button name="Botão teste props" />
-        < Button name="Botão teste props" />
-        < Button name="Botão teste props" />
+          < Button name="Botão teste props" />
+          < Button name="Botão teste props" />
+          < Button name="Botão teste props" />
+          < Button name="Botão teste props" />
         </section>
         
-        < Button name="Botão teste props" />
     </header>
   )
 }
